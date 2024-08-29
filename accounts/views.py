0c521d4c83b56dbf3f171306455f487b3d18c542
SHY-89 <tjduwkrn@gmail.com>
2024-08-29 20:34:14 +0900

    feat accouts/account
    login, logout complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import MyAuthenticationForm
+from django.views.decorators.http import require_http_methods, require_POST
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
 # Create your views here.
+@require_http_methods(["GET", "POST"])
+def login(request):
+    if request.method == "POST":
+        form = MyAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_page = request.GET.get("next") or "home"
+            return redirect(next_page)
+    else:
+        form = MyAuthenticationForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", context)
+
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect("home")
