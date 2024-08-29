0ce8bd92b7e4e6a0e35da86bad8fc5069589c45b
SHY-89 <tjduwkrn@gmail.com>
2024-08-29 20:54:28 +0900

    feat accouts/account  signup complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,11 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
+
 
 class MyAuthenticationForm(AuthenticationForm):
     pass
 
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ("username", )
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-from accounts.forms import MyAuthenticationForm
-from django.views.decorators.http import require_http_methods, require_POST
-from django.contrib.auth import login as auth_login, logout as auth_logout
+from accounts.forms import MyAuthenticationForm, MyUserCreationForm
+from django.views.decorators.http import (
+    require_http_methods, 
+    require_POST,
+)
+from django.contrib.auth import (
+    login as auth_login, 
+    logout as auth_logout,
+)
 
 # Create your views here.
 @require_http_methods(["GET", "POST"])
 def login(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    
     if request.method == "POST":
         form = MyAuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -24,3 +33,22 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("home")
+
+
+@require_http_methods(["GET", "POST"])
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("home") 
+    else:
+        form = MyUserCreationForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/signup.html", context)
